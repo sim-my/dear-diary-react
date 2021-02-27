ad8d8af26c98497d75aed4f870e7d551d8f112d5
sim-my <simranpanthi101@gmail.com>
2021-02-27 20:19:55 +0545

    Completed reouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
 
+import Login from "./views/Login";
+import Dashboard from "./views/Dashboard";
+import CreatePost from "./views/CreatePost";
+import NotFound from "./views/404";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Register from "./views/Register";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          <Route path="/create">
+            <CreatePost />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/404">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
